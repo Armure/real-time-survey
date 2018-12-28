f14c827e1c259546fc9b55611759d20e9104893d
Jordan Munroe <jmunroe@spinvfx.com>
2018-12-28 16:07:30 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+
+const port = process.env.PORT || 4001
+const index = require('./routes/index')
+
+const app = express()
+app.use(index)
+
+const server = http.createServer(app)
+
+const io = socketIo(server)
+
+const state = {
+  clients: [],
+  length: 0
+}
+
+const onChange = socket => {
+  if (socket) return socket.emit('change', state)
+  io.emit('change', state)
+}
+
+const setLength = (socket, length) => {
+  state.length = length
+  state.clients.forEach(client => client.selection = null)
+  onChange()
+}
+
+const handleConnect = ({ id }) => {
+  console.log(`New client connected with id: ${id}`)
+  state.clients.push({ id, selection: null })
+  onChange()
+}
+
+const handleDisconnect = ({ id }) => {
+  console.log(`Client ${id} disconnected`)
+  state.clients = state.clients.filter(client => client.id !== id)
+}
+
+const handleSelection = ({ id }, value) => {
+  const client = state.clients.find(c => c.id === id)
+  client.selection = value
+  onChange()
+}
+
+io.on('connection', socket => {
+  handleConnect(socket)
+  socket.on('disconnect', () => handleDisconnect(socket))
+  socket.on('setLength', length => setLength(socket, length))
+  socket.on('makeSelection', value => handleSelection(socket, value))
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
